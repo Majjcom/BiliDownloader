606b961f8083206fe0ce0765f283e1aeedf55aa1
Majjcom <majjcom@126.com>
2023-09-06 21:17:03 +0800

    server fix

--- server/server.py
+++ server/server.py
@@ -14,20 +14,28 @@ def logstr(*st):
     sts = []
     for i in st:
         sts.append(str(i))
-    ret = ' '.join(sts)
+    ret = " ".join(sts)
     return ret
 
 
-def log(s: str, level: str = 'info'):
+def log(s: str, level: str = "info"):
     global logcount
     if logcount == 0 or logcount > 10000:
         global logfile
         if logfile:
             logfile.close()
-        logfile = open('./logs/{}.log'.format(datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')), 'w', buffering=1)
+        logfile = open(
+            "./logs/{}.log".format(
+                datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+            ),
+            "w",
+            buffering=1,
+        )
         sys.stdout = logfile
         logcount = 0
-    prstr = '[{}][{}] '.format(datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S'), level)
+    prstr = "[{}][{}] ".format(
+        datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S"), level
+    )
     prstr += s
     print(prstr)
     logcount += 1
@@ -39,73 +47,56 @@ class Runner(thr.Thread):
         self._l: server.Linker = l
 
     def run(self):
-        log('Linker: {}'.format(self._l.getAddr()))
+        log("Linker: {}".format(self._l.getAddr()))
         try:
             get = self._l.recvMsg()
-            log('Action: {}'.format(get))
-            if get['action'] == 'version':
-                if 'after' in get:
-                    self._l.sendMsg({'content': getVersionAfter()})
+            log("Action: {}".format(get))
+            if get["action"] == "version":
+                if "after" in get:
+                    self._l.sendMsg({"content": getVersionAfter()})
                 else:
-                    self._l.sendMsg({'content': getVersion()})
-            elif get['action'] == 'updateInfo':
-                self._l.sendMsg({'content': getUpdateInfo(ver_from=get['bVersion'])})
-            elif get['action'] == 'updateUrl':
+                    self._l.sendMsg({"content": getVersion()})
+            elif get["action"] == "updateInfo":
+                self._l.sendMsg({"content": getUpdateInfo()})
+            elif get["action"] == "updateUrl":
                 get_info = getUpdateUrl()
-                self._l.sendMsg({'content': {'url': get_info[0], 'hash': get_info[1]}})
+                self._l.sendMsg({"content": {"url": get_info[0], "hash": get_info[1]}})
             time.sleep(0.2)
             self._l.close()
         except:
-            log(logstr(sys.exc_info()[0], sys.exc_info()[1]), 'Exception')
+            log(logstr(sys.exc_info()[0], sys.exc_info()[1]), "Exception")
             try:
                 self._l.close()
             except:
-                log('Close Error...', 'Exception')
+                log("Close Error...", "Exception")
 
 
 def getVersion():
     get = str()
-    with open('./data/ver', 'r') as f:
+    with open("./data/ver", "r") as f:
         get = f.read()
     return get
+
 
 def getVersionAfter():
     get = str()
-    with open('./data/ver', 'r') as f:
+    with open("./data/ver", "r") as f:
         get = f.read()
     return get
 
-def getUpdateInfo(ver_from='0.0.0', ver_to=None):
-    if ver_to is None:
-        ver_to = getVersionAfter()
+
+def getUpdateInfo():
     get = str()
-    with open('./data/updateInfo', 'r', encoding='utf_8') as f:
+    with open("./data/updateInfo", "r", encoding="utf_8") as f:
         get = f.read()
-    head = get.split('\n-', 1)[0]
-    ver_from_re = ''
-    ver_to_re = ''
-    for i in ver_from:
-        if i in '-?.+,$*[]{}\\':
-            ver_from_re += '\\'
-        ver_from_re += i
-    for i in ver_to:
-        if i in '-?.+,$*[]{}\\':
-            ver_to_re += '\\'
-        ver_to_re += i
-    re_p = r'\- ' + ver_to_re + r'.+(?=\n\n\- ' + ver_from_re + r')'
-    searched = re.compile(re_p, flags=re.S).search(get)
-    if searched is None:
-        return head
-    body = searched.group()
-    ret = head + '\n' + body
-    return ret
+    return get
 
 
 def getUpdateUrl():
     get = list()
-    with open('./data/updateUrl', 'r') as f:
+    with open("./data/updateUrl", "r") as f:
         get.append(f.read())
-    with open('./data/updateHash', 'r') as f:
+    with open("./data/updateHash", "r") as f:
         get.append(f.read())
     return tuple(get)
 
@@ -119,14 +110,13 @@ def main():
             run.start()
         except KeyboardInterrupt:
             global logfile
-            log('stop...')
+            log("stop...")
             if logfile:
                 logfile.close()
             break
         except:
-            log('Connection create error...', 'Exception')
+            log("Connection create error...", "Exception")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
