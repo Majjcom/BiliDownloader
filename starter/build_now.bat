c631256090a89b72e4636eaff45536551bf34d7f
Majjcom <majjcom@126.com>
2024-05-31 17:24:07 +0800

    update 1.2.2

--- starter/build_now.bat
+++ starter/build_now.bat
@@ -1,5 +1,7 @@
 @echo off
 
+chcp 65001
+
 call "D:\Program Files\Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Build\vcvars64.bat"
 
 @echo Start Build...
@@ -7,5 +9,3 @@ call "D:\Program Files\Microsoft Visual Studio\2022\Professional\VC\Auxiliary\Bu
 devenv %cd%\starter.sln /Rebuild "Release|x86"
 
 @echo Build finish...
-
-PAUSE
