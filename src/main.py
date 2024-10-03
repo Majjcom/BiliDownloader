ec656b3904a31f1ea784b893b56c6fba0fe59582
Majjcom <majjcom@126.com>
2024-10-03 21:13:41 +0800

    update 1.3.2

--- src/main.py
+++ src/main.py
@@ -10,6 +10,10 @@ if __name__ == "__main__":
     os.environ["QT_ENABLE_HIGHDPI_SCALING"] = "0"
     app = QtWidgets.QApplication(sys.argv)
 
+    # change cwd when not meet the requirements
+    if os.path.samefile(os.path.split(__file__)[0], os.getcwd()):
+        os.chdir(os.path.split(os.getcwd())[0])
+
     # update styles
     style = configUtils.getUserData(configUtils.Configs.QT_STYLE, "default")
     if style != "default":
